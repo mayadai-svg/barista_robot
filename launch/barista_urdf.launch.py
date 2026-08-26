34cedf87e3ae0026631b28d0d1edceefb0e7b72e
mdai <maya.dai@eteration.com>
2026-08-26 20:53:01 +0000

    Fix laser_scan_link_joint and laser_scan_model_link_joint, ros2_control_config and config files to ensure launch file runs

--- launch/barista_urdf.launch.py
+++ launch/barista_urdf.launch.py
@@ -1,21 +1,37 @@
 import os
 
-from ament_index_python.packages import get_package_share_directory
+from ament_index_python.packages import (get_package_prefix, get_package_share_directory)
 from launch import LaunchDescription
-from launch.substitutions import Command
-from launch_ros.actions import Node
+from launch.actions import (DeclareLaunchArgument, IncludeLaunchDescription)
+from launch.substitutions import (PathJoinSubstitution, Command, LaunchConfiguration)
+from launch_ros.actions import (Node, SetParameter)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 # this is the function launch  system will look for
 def generate_launch_description():
 
-    ####### DATA INPUT ##########
     urdf_file = 'barista_robot_model.urdf'
     #xacro_file = "urdfbot.xacro"
     package_description = "barista_robot_description"
 
-    ####### DATA INPUT END ##########
+    # Get Package Directory
+    pkg_robot_description = get_package_share_directory('barista_robot_description')
+    gz_sim_pkg = get_package_share_directory("ros_gz_sim")
+
     print("Fetching URDF ==>")
     robot_desc_path = os.path.join(get_package_share_directory(package_description), "urdf", urdf_file)
+
+    # Set the Path to Robot Mesh Models for Loading in Gazebo Sim
+    install_dir_path_description = (get_package_prefix('barista_robot_description') + "/share")
+    description_meshes_path = os.path.join(pkg_robot_description, "meshes")
+    gazebo_resource_paths = [install_dir_path_description, description_meshes_path]
+    
+    if "GZ_SIM_RESOURCE_PATH" in os.environ:
+        for resource_path in gazebo_resource_paths:
+            if resource_path not in os.environ["GZ_SIM_RESOURCE_PATH"]:
+                os.environ["GZ_SIM_RESOURCE_PATH"] += (':' + resource_path)
+    else:
+        os.environ["GZ_SIM_RESOURCE_PATH"] = (':'.join(gazebo_resource_paths))
 
     # Robot State Publisher
     robot_state_publisher_node = Node(
@@ -39,10 +55,77 @@ def generate_launch_description():
             arguments=['-d', rviz_config_dir])
 
 
+    # -------------- Start the World -------------------
+    # Setup to launch the simulator and Gazebo world
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(gz_sim_pkg, 'launch', 'gz_sim.launch.py')),
+            launch_arguments={'gz_args': [
+            '-r ',  # <-- start unpaused
+            PathJoinSubstitution([pkg_robot_description, 'worlds', 'empty.world'])
+        ]}.items(),
+    )
+
+
+    # ----------------- Spawn the Robot -----------------
+    declare_spawn_model_name = DeclareLaunchArgument("model_name", default_value="my_robot",
+                                                     description="Model Spawn Name")
+    declare_spawn_x = DeclareLaunchArgument("x", default_value="0.0",
+                                            description="Model Spawn X Axis Value")
+    declare_spawn_y = DeclareLaunchArgument("y", default_value="0.0",
+                                            description="Model Spawn Y Axis Value")
+    declare_spawn_z = DeclareLaunchArgument("z", default_value="0.2",
+                                            description="Model Spawn Z Axis Value")
+    declare_spawn_yaw = DeclareLaunchArgument("yaw", default_value="3.14",
+                                            description="Model Spawn Yaw Value")
+    gz_spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        name="my_robot_spawn",
+        arguments=[
+            "-name", LaunchConfiguration("model_name"),
+            "-allow_renaming", "true",
+            "-topic", "robot_description",
+            "-x", LaunchConfiguration("x"),
+            "-y", LaunchConfiguration("y"),
+            "-z", LaunchConfiguration("z"),
+            "-Y", LaunchConfiguration("yaw"),
+        ],
+        output="screen",
+    )
+
+    # ROS-Gazebo Bridge #
+    gz_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        name="gz_bridge",
+        arguments=[
+            "/clock" + "@rosgraph_msgs/msg/Clock" + "[gz.msgs.Clock",
+            "/cmd_vel" + "@geometry_msgs/msg/Twist" + "@gz.msgs.Twist",
+            "/tf" + "@tf2_msgs/msg/TFMessage" + "[gz.msgs.Pose_V",
+            "/odom" + "@nav_msgs/msg/Odometry" + "[gz.msgs.Odometry",
+            "/joint_states" + "@sensor_msgs/msg/JointState" + "[gz.msgs.Model",
+            "/scan" + "@sensor_msgs/msg/LaserScan" + "[gz.msgs.LaserScan"
+        ],
+        remappings=[
+            # there are no remappings for this robot description
+        ],
+        output="screen",
+    )
+
     # create and return launch description object
     return LaunchDescription(
         [            
+            SetParameter(name="use_sim_time", value=True),
+            declare_spawn_model_name,
+            declare_spawn_x,
+            declare_spawn_y,
+            declare_spawn_z,
+            declare_spawn_yaw,
             robot_state_publisher_node,
-            rviz_node
+            rviz_node,
+            gz_sim,
+            gz_spawn_entity,
+            gz_bridge
         ]
     )
